a9700671cc681f7bd0009337a1fdee2abf1c6746
Shubh Sheth <shubhsheth8@gmail.com>
2020-04-05 01:34:07 -0500

    Added Update and Controls

--- app.js
+++ app.js
@@ -55,5 +55,32 @@ function create() {
 }
 
 function update() {
+    game.physics.arcade.collide(player, platforms)
+    game.physics.arcade.collide(diamonds, platforms)
+    game.physics.arcade.overlap(player, diamonds, collectDiamonds, null, this)
 
+    player.body.velocity.x = 0
+
+    //controls
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150
+        player.animations.play('left')
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150
+        player.animations.play('right')
+    } else {
+        player.animations.stop()
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -400
+    }
+
+}
+
+function collectDiamonds(player, diamond) {
+    diamond.kill()
+
+    score += 10
+    scoreText.text = 'Score: ' + score
 }
